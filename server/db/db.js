61fc4d542d20f6dd7d90f7616ed3487272e4caf9
NEUmmc <bmc.bob@qq.com>
2020-06-30 11:30:19 +0800

    第一次整体上传

---
+++ server/db/db.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mysql: {
+        host: 'localhost',
+        user: 'root',
+        password: '239274',
+        port: '3306',
+        database: 'data'
+    }
+}
---
+++ server/db/sqlMap.js
@@ -0,0 +1,8 @@
+var sqlMap = {
+    admin: {
+        select: 'select * from admin where username = ?'
+    }
+
+}
+
+module.exports = sqlMap
