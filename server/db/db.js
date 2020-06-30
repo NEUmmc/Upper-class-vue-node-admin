0b0c4897648290397e8f0c8790c864b86cdefa86
NEUmmc <46310131+NEUmmc@users.noreply.github.com>
2020-06-30 11:38:57 +0800

    更新数据库配置信息

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,9 @@
 module.exports = {
     mysql: {
         host: 'localhost',
-        user: 'root',
-        password: '239274',
+        user: '你的用户名',
+        password: '你的密码',
         port: '3306',
-        database: 'data'
+        database: '你的数据库名'
     }
 }
